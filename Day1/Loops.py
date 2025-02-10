9a94695c53c80e3baa618e94312996594cac6c7d
SabehShaikh <sabehshaikh201@gmail.com>
2025-02-11 00:20:03 +0500

    add day1 practice code.

--- Day1/Loops.py
+++ Day1/Loops.py
@@ -11,3 +11,15 @@ j = 15
 while j <= 20:
     print("value of j: ", j)
     j += 1
+
+# break statement
+for num in range(1, 11):
+    if num == 5:
+        break
+    print(num)
+
+# continue statement 
+for k in range(1,5):
+    if k == 3:
+        continue
+    print(k)
